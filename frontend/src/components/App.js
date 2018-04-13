3c7bd3bb4e5dd7075467cb58d697fee119a2434d
Stani2980 <stani2980@hotmail.com>
2018-04-13 12:40:07 +0200

    added err

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,7 +4,7 @@ import {
   Route,
   Switch
 } from 'react-router-dom'
-
+import "../style/App.css";
 
 import facade from "../apiFacade";
 import LogIn from './LogIn';
@@ -17,43 +17,44 @@ import NoMatch from './NoMatch'
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { loggedIn: false }
+    this.state = { loggedIn: false, error: "" }
   }
-  
+
   logout = () => {
     facade.logout();
     this.setState({ loggedIn: false });
   }
 
   login = (user, pass) => {
-    facade.login(user,pass)
-     .then(res =>this.setState({ loggedIn: true }))
-     .catch(err => console.log(err));
+    facade.login(user, pass)
+      .then(res => this.setState({ loggedIn: true, error: "" }))
+      .catch(err => this.setState({ error: JSON.stringify(err) }));
   }
 
   render() {
     return (
       <div>
-          <Router>
-            <Switch>
-              <Route exact path='/' render={() => {
-                return (
-                  // if user isnt logged in return, and render, the login screen
-                  // if logged in return home screen
+        <Router>
+          <Switch>
+            <Route exact path='/' render={() => {
+              return (
+                // {/* // if user isnt logged in return, and render, the login screen
+                // // if logged in return home screen */}
                 !this.state.loggedIn ? (<LogIn login={this.login} />) :
-                ( <div>
-                    <LoggedIn facade={facade}/>
+                  (<div>
+                    <LoggedIn facade={facade} />
                     <button onClick={this.logout}>Logout</button>
                   </div>)
-                )
-              }}/>
+              )
+            }} />
 
-              {/* render NoMatch if no other Route components match
+            {/* render NoMatch if no other Route components match
               the URL */}
-              <Route path='/people' render={() => <PeopleTable facade={facade}/>}/>
-              <Route component={NoMatch}/>
-            </Switch>
-          </Router>
+            <Route path='/people' render={() => <PeopleTable facade={facade} />} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+        <p className="App-error">{this.state.error}</p>
       </div>
     )
   }
